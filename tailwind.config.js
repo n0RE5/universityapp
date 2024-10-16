c70aaee8dbe526ad6a18ecf77e74348fd8d2bb2c
MH <mail@mail.com>
2024-10-16 15:58:17 +0300

    [~] Dark/Light theme updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        dark: { raw: '(prefers-color-scheme: dark)' },
+      },
+    },
   },
   plugins: [],
 };
